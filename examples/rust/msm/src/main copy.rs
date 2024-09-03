6a31a399dd8cddb0fffb86beb6726b4fd8206aa0
cd4761 <cd4761@gmail.com>
2024-09-04 00:23:46 +0900

    update

--- examples/rust/msm/src/main copy.rs
+++ examples/rust/msm/src/main copy.rs
@@ -1,4 +1,4 @@
-use icicle_bn254::curve::{CurveCfg, G1Projective, G2CurveCfg, G2Projective, ScalarCfg};
+use icicle_bls12_381::curve::{CurveCfg, G1Projective, ScalarCfg};
 
 use icicle_cuda_runtime::{
     memory::{DeviceVec, HostSlice},
@@ -63,15 +63,15 @@ fn main() {
     let upper_size = 1 << (upper_bound);
     println!("Generating random inputs on host for bn254...");
     let upper_points = CurveCfg::generate_random_affine_points(upper_size);
-    let g2_upper_points = G2CurveCfg::generate_random_affine_points(upper_size);
+    // let g2_upper_points = G2CurveCfg::generate_random_affine_points(upper_size);
     let upper_scalars = ScalarCfg::generate_random(upper_size);
 
     // println!("lower_bound: {:?}", lower_bound);
-    // println!("upper_bound: {:?}", upper_bound);
+    // println!("upper_bound: {:?}", upper_bound);///
 
-    println!("upper_points: {:?}", &upper_points[..10]);
+    // println!("upper_points: {:?}", &upper_points[..10]);
     // println!("g2_upper_points: {:?}", g2_upper_points);
-    println!("upper_scalars: {:?}", &upper_scalars[..10]);
+    // println!("upper_scalars: {:?}", &upper_scalars[..10]);
 
     for i in lower_bound..=upper_bound {
         let log_size = i;
@@ -82,69 +82,70 @@ fn main() {
         );
         // Setting Bn254 points and scalars
         let points = HostSlice::from_slice(&upper_points[..size]);
-        let g2_points = HostSlice::from_slice(&g2_upper_points[..size]);
+        // let g2_points = HostSlice::from_slice(&g2_upper_points[..size]);
         let scalars = HostSlice::from_slice(&upper_scalars[..size]);
         // let scalars_sample = HostSlice::from_slice(&upper_scalars[..10]);
         // let points_sample = HostSlice::from_slice(&upper_points[..10]);
 
-        // println!("points: {:?}", points_sample);
+        println!("points: {:?}", points[0]);
         // println!("g2_points: {:?}", g2_points[0]);
         // println!("scalars: {:?}", scalars_sample);
         
-        println!("Configuring bn254 MSM...");
+        println!("Configuring bls12-381 MSM...");
         let mut msm_results = DeviceVec::<G1Projective>::cuda_malloc(1).unwrap();
         // let mut msm_results_sample = DeviceVec::<G1Projective>::cuda_malloc(1).unwrap();
-        let mut g2_msm_results = DeviceVec::<G2Projective>::cuda_malloc(1).unwrap();
+        // let mut g2_msm_results = DeviceVec::<G2Projective>::cuda_malloc(1).unwrap();
         let stream = CudaStream::create().unwrap();
-        let g2_stream = CudaStream::create().unwrap();
+        // let g2_stream = CudaStream::create().unwrap();
         let mut cfg = msm::MSMConfig::default();
-        let mut g2_cfg = msm::MSMConfig::default();
+        // let mut g2_cfg = msm::MSMConfig::default();
         cfg.ctx
             .stream = &stream;
-        g2_cfg
-            .ctx
-            .stream = &g2_stream;
+        // g2_cfg
+        //     .ctx
+        //     .stream = &g2_stream;
         cfg.is_async = true;
-        g2_cfg.is_async = true;
+        // g2_cfg.is_async = true;
 
         // msm::msm(scalars_sample, points_sample, &cfg, &mut msm_results_sample[..]).unwrap();
 
-        println!("Executing bn254 MSM on device...");
+        println!("Executing bls12-381 MSM on device...");
         #[cfg(feature = "profile")]
         let start = Instant::now();
         msm::msm(scalars, points, &cfg, &mut msm_results[..]).unwrap();
         #[cfg(feature = "profile")]
         println!(
-            "ICICLE BN254 MSM on size 2^{log_size} took: {} ms",
+            "ICICLE bls12-381 MSM on size 2^{log_size} took: {} ms",
             start
                 .elapsed()
                 .as_millis()
         );
-        msm::msm(scalars, g2_points, &g2_cfg, &mut g2_msm_results[..]).unwrap();
+        // msm::msm(scalars, g2_points, &g2_cfg, &mut g2_msm_results[..]).unwrap();
 
         println!("Moving results to host..");
         let mut msm_host_result = vec![G1Projective::zero(); 1];
-        let mut g2_msm_host_result = vec![G2Projective::zero(); 1];
+        // let mut g2_msm_host_result = vec![G2Projective::zero(); 1];
 
         stream
             .synchronize()
             .unwrap();
-        g2_stream
-            .synchronize()
-            .unwrap();
+        // g2_stream
+        //     .synchronize()
+        //     .unwrap();
         msm_results
             .copy_to_host(HostSlice::from_mut_slice(&mut msm_host_result[..]))
             .unwrap();
-        g2_msm_results
-            .copy_to_host(HostSlice::from_mut_slice(&mut g2_msm_host_result[..]))
-            .unwrap();
-        println!("bn254 result: {:#?}", msm_host_result);
-        println!("G2 bn254 result: {:#?}", g2_msm_host_result);
+        // g2_msm_results
+        //     .copy_to_host(HostSlice::from_mut_slice(&mut g2_msm_host_result[..]))
+        //     .unwrap();
+        println!("bls12-381 result: {:#?}", msm_host_result);
+      
+        // println!("G2 bls12-381 result: {:#?}", g2_msm_host_result);
 
         #[cfg(feature = "lambdaworks")]
         {
             println!("Checking against lambdaworks...");
-            let lamda_points: Vec<G1Point> = upper_points
+            let lamda_points: Vec<G1Point> = upper_points[..size]
                 .iter()
                 .map(|p| G1Point::new([
                     FieldElement::from_hex(&p.x.to_string()).unwrap(),
@@ -152,8 +153,10 @@ fn main() {
                     FieldElement::one(),
                 ]))
                 .collect();
+            
+            println!("lambda points: {:?}", lamda_points[0]);
 
-            let lamda_scalars: Vec<UnsignedInteger<4>> = upper_scalars
+            let lamda_scalars: Vec<UnsignedInteger<4>> = upper_scalars[..size]
                 .iter()
                 .map(|s| UnsignedInteger::<4>::from_hex(&s.to_string()).unwrap())
                 .collect();
